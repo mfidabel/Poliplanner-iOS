2cb0c68b1d88d9a8021e6616924a96cfc1688d39
Mateo Fidabel <mateofidabel@gmail.com>
2020-11-25 16:30:24 -0300

    Initial Commit

---
+++ Poliplanner/PoliplannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoliplannerApp.swift
+//  Poliplanner
+//
+//  Created by Mateo Fidabel on 11/25/20.
+//
+
+import SwiftUI
+
+@main
+struct PoliplannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
